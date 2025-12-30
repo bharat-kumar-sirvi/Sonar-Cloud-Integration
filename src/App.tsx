46336e10df2458cb01984b34a013d7a9d36e0c7c
Bharat Kumar <bharat.kumar@zemosolabs.com>
2025-12-30 14:26:57 +0530

    Add Sonar Cloud integration
    fix: Sonar Cloud Configuration
    
    feat: Add Sonar Cloud Configuration
    
    fix: Change App File
    
    fix: Change App File
    
    fix: Change Sonar Configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App({ flag }: { flag: boolean }) {
+  if (flag == true) {
+    console.log("Sonar Cloud");
+  }
+  console.log("Sonar Cloud");
+  console.log("Sonar Cloud");
+  console.log("Sonar Cloud");
+  const temp = 123;
+  var a = 10;
+  return (
+    <>
+      <h1>Sonar Cloud + {flag}</h1>
+    </>
+  );
+}
+
+export default App;
